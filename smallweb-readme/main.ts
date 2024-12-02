eab0ac7622ca506edff6a836d4e7811eccad3ecd
pomdtr <achille.lacoin@gmail.com>
2024-12-02 01:04:50 +0100

    2 Dec 2024, 01:04

--- smallweb-readme/main.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    fetch: (req: Request) => {
-        const url = new URL(req.url);
-        url.hostname = "www.smallweb.run";
-        return Response.redirect(url, 301);
-    },
-};
